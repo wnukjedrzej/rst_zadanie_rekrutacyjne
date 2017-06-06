f642d428c4e918a63da3f6662d49c13747c102cd
jedras <jedrek_wnuk@wp.pl>
2017-06-06 11:57:08 +0200

    Czysty projekt

---
+++ RST_zadanie_rekrutacyjne/app/src/main/java/wnukjedrzej/rst_zadanie_rekrutacyjne/MainActivity.java
@@ -0,0 +1,13 @@
+package wnukjedrzej.rst_zadanie_rekrutacyjne;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
